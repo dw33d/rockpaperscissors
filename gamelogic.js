03017c4f71d25b16dd610864a8e3fd119cae89fe
dw33d <170299648+dw33d@users.noreply.github.com>
2024-06-15 22:04:57 +0200

    Lots of bugs, gotta fix them

--- gamelogic.js
+++ gamelogic.js
@@ -1,9 +1,15 @@
 const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
+let humanChoice = window.prompt("Input your choice(rock, paper, scissors)"); 
+const computerOptions = [rock, paper, scissors];  // I want to store these variables in an array and have them selected at random 
 
+let humanScore = 0;
+let pcScore = 0;
+
+
+// Will return the compute rcode based on random selection
 function getComputerChoice(){
-    const computerOptions = [rock, paper, scissors];  // I want to store these variables in an array and have them selected at random 
     return computerOptions[(Math.floor(Math.random() * computerOptions.length))]; // This should get a random option for me
 }
 
@@ -12,7 +18,28 @@ console.log(getComputerChoice());
 
 // WIll ask for human input and then log it to the console
 function getHumanChoice(){
-    let humanChoice = window.prompt("Input your choice(rock, paper, scissors)"); 
     return humanChoice;
 }
 console.log(getHumanChoice())
+
+function roundPlay(humanChoice, computerOptions){
+
+    if(humanChoice === "rock" && computerOptions === "paper"){
+        console.log("It's a rock and a paper!")
+    } else {
+        console.log("Nah...not it")
+    }
+ 
+
+
+
+}
+
+const humanSelection = humanChoice;
+const computerSelection = computerOptions;
+
+roundPlay(humanSelection, computerSelection)
+
+function play(){
+    
+}
