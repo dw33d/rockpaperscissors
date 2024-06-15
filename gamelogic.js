b9b67264c42f06c35a40210f1b7d13afa82f1b4b
dw33d <170299648+dw33d@users.noreply.github.com>
2024-06-15 21:00:03 +0200

    Fixing some errors

--- gamelogic.js
+++ gamelogic.js
@@ -2,12 +2,17 @@ const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
 
-const computerOptions = [rock, paper, scissors]  // I want to store these variables in an array and have them selected at random 
-
 function getComputerChoice(){
-
+    const computerOptions = [rock, paper, scissors];  // I want to store these variables in an array and have them selected at random 
     return computerOptions[(Math.floor(Math.random() * computerOptions.length))]; // This should get a random option for me
 }
 
 getComputerChoice();
 console.log(getComputerChoice());
+
+// WIll ask for human input and then log it to the console
+function getHumanChoice(){
+    let humanChoice = window.prompt("Input your choice(rock, paper, scissors)"); 
+    return humanChoice;
+}
+console.log(getHumanChoice())
